cdf22d104a54f080b2c2cfc351d77f919d640da8
Lahoanganh0708 <lahoanganh2004@gmail.com>
2026-01-22 15:27:29 +0700

    chapter 3

--- odoo-19.0/addons/estate/__manifest__.py
+++ odoo-19.0/addons/estate/__manifest__.py
@@ -1,4 +1,12 @@
 {
     'name': 'Real Estate Advertisement',
+    'version': '1.0',
+    'author': 'Hoang Anh',
+    'category': 'Tutorial',
+    'summary': 'Real Estate Module',
     'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
 }
