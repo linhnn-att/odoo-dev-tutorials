9f3e160e816e2ab3c5ee983610bf55faba8e3735
Lahoanganh0708 <lahoanganh2004@gmail.com>
2026-01-23 09:33:01 +0700

    chapter 5

--- odoo-19.0/addons/estate/__manifest__.py
+++ odoo-19.0/addons/estate/__manifest__.py
@@ -7,9 +7,10 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
 }
-
--- odoo-19.0/addons/estate/models/estate_property.py
+++ odoo-19.0/addons/estate/models/estate_property.py
@@ -1,27 +1,34 @@
 from odoo import fields, models
+from datetime import timedelta
+from odoo.tools import date_utils
+
 
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
 
-    name = fields.Char(
-        required=True
-    )
+    active = fields.Boolean(default=True)
+
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(
-        required=True
+    date_availability = fields.Date(
+        copy=False,
+        default=lambda self: date_utils.add(fields.Date.today(), months=3)
     )
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(
+        readonly=True,
+        copy=False
+    )
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        selection=[
+        [
             ('north', 'North'),
             ('south', 'South'),
             ('east', 'East'),
@@ -29,3 +36,15 @@ class EstateProperty(models.Model):
         ]
     )
 
+    state = fields.Selection(
+        [
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('cancelled', 'Cancelled'),
+        ],
+        required=True,
+        copy=False,
+        default='new'
+    )
